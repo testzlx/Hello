62531812208ed0e58df871cc72adad9dda4bab8e
testzlx <709472699@qq.com>
2022-05-24 20:57:35 +0800

    c语言编程

--- src/main/main.c
+++ src/main/main.c
@@ -1,9 +1,11 @@
 //
 // Created by zhanglinxing on 2021/6/30.
 //
-#include <stdio.h>
 #include "../include/foo.h"
 #include "../include/bar.h"
+
+
+
 int main(int argc, char *argv[])
 {
     printf("main +\n");
